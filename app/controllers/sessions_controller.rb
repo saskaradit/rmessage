477e7b7f07b6bb1f6b1bae9a81273b47ca8f55d1
Rad Saskara <radsaskara@Rads-MacBook-Pro.local>
2020-12-14 16:27:41 +0700

    adding login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
     def new
     end
+
+    def create
+        user = User.find_by(username:params[:session][:username.downcase])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "You Are Logged In"
+            redirect_to root_path
+        else
+            flash.now[:alert] = "Invalid Credentials"
+            render 'new'
+        end
+    end
+
 end
